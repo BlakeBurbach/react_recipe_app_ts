b08b1f985e2a4033a840f0311bc8dc3f2b8feb86
Blake Burbach <cras.bburbach@gmail.com>
2018-09-12 17:11:51 -0500

    more styling and about to implement react-router

--- src/components/Recipes/Recipes.tsx
+++ src/components/Recipes/Recipes.tsx
@@ -11,7 +11,9 @@ const Recipes: React.SFC<IRecipesProps> = props => (
             return (
                 <div className="recipes__card" key={recipe.recipe_id}>
                     <img className="recipes__card-img" src={recipe.image_url} alt={recipe.title} />
-                    <p className="t-recipes__title recipes__text">{recipe.title}</p>
+                    <p className="t-recipes__title recipes__text">
+                        { recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...` }
+                    </p>
                     <p className="t-recipes__subtitle recipes__text">Publisher: <span className="t-recipes__subtitle-span recipes__text">{recipe.publisher}</span>
                     </p>
                     <button className="t-recipes__button recipes__button">View Recipe</button>
