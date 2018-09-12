b63f08ec403ce391ec5c8a92d554dd8101ea7b6b
Blake Burbach <cras.bburbach@gmail.com>
2018-09-12 16:01:04 -0500

    Fixed some css values to be more semantic

--- src/components/RecipeSearchForm/RecipeSearchForm.tsx
+++ src/components/RecipeSearchForm/RecipeSearchForm.tsx
@@ -7,9 +7,9 @@ interface IRecipeSearchFormProps {
 
 const RecipeSearchForm: React.SFC<IRecipeSearchFormProps> = props => (
     <form className="App__input-form" onSubmit={props.getRecipes}>
-        <p className="t-input-label App__input-form__label">Search by Ingredient</p>
-        <input className="t-input-text App__input-form__input" type="text" name="RecipeName" placeholder="Recipe..."/>
-        <button className="t-form-button App__input-form__button">Search Recipes</button>
+        <p className="t-input-label">Search by Ingredient</p>
+        <input className="t-input-text input-form__input" type="text" name="RecipeName" placeholder="Recipe..."/>
+        <button className="t-form-button input-form__button">Search Recipes</button>
     </form>
 );
 
