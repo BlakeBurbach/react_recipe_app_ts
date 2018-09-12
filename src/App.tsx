9fdbe6af4faa40a99f5cdf7c1f7ea9b68e4b2044
Blake Burbach <cras.bburbach@gmail.com>
2018-09-12 12:26:29 -0500

    RecipeSearchForm styling, and now receiving data from API

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,18 @@ import './App.css';
 import Header from './components/Header/Header';
 import RecipeSearchForm from './components/RecipeSearchForm/RecipeSearchForm';
 
+const API_KEY = "17963c4e9b3d709bed93a750fdd4f7ea";
+
 class App extends React.Component {
 
+  // fetch function called by onSubmit from RecipeSearchForm to Fork2Food API
   public getRecipes = async (event: any) => {
     event.preventDefault();
-    console.log('getRecipes working');
+    // value from text input
+    const RecipeName = event.target.RecipeName.value;
+    const API_CALL = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${RecipeName}`);
+    const data = await API_CALL.json();
+    console.log(data);
   }
 
   public render() {
--- src/components/RecipeSearchForm/RecipeSearchForm.tsx
+++ src/components/RecipeSearchForm/RecipeSearchForm.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
+import './RecipeSearchForm.css';
 
 interface IRecipeSearchFormProps {
     getRecipes: (event: any) => Promise<void>
 };
 
 const RecipeSearchForm: React.SFC<IRecipeSearchFormProps> = props => (
-    <form onSubmit={props.getRecipes}>
-        <p>Search by Ingredient</p>
-        <input type="text" name="RecipeName" placeholder="Recipe..."/>
-        <button>Search Recipes</button>
+    <form className="App__input-form" onSubmit={props.getRecipes}>
+        <p className="t-input-label App__input-form__label">Search by Ingredient</p>
+        <input className="t-input-text App__input-form__input" type="text" name="RecipeName" placeholder="Recipe..."/>
+        <button className="t-form-button App__input-form__button">Search Recipes</button>
     </form>
 );
 
